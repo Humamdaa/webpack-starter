7bb40459753ad33d7360dcd74f4624d4c73deffc
bhomaidan <bhomaidan@hotmail.com>
2025-01-25 10:07:23 +0100

    fix singlequote

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "no-console": "warn",
-    "no-unused-vars": "warn",
-    semi: ["error", "always"],
-    quotes: ["error", "single"],
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
   },
 };
